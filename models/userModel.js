b68270f20594905a9e684639e683e1c8b3b8331d
NallyTHEdude <rifaqatnawaz25@gmail.com>
2026-02-18 08:03:58 +0000

    added a user model

---
+++ models/userModel.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, "Please Provide a username"],
+        unique: true,
+    },
+
+    email: {
+        type: String,
+        required: [true, "Please Provide an email"],
+        unique: true,
+    },
+
+    password: {
+        type: String,
+        required: [true, "Please Provide a password"],
+    },
+
+    isVerified: {
+        type: Boolean,
+        default: false,
+    },
+
+    isAdmin:{
+        type: Boolean,
+        default: false,
+    },
+
+    forgotPasswordToken: String,
+
+    forgotPasswordTokenExpiry: Date,
+
+    verificationToken: String,
+    
+    verificationTokenExpiry: Date,
+    
+})
+
+const User = mongoose.model.users || mongoose.model("users", userSchema);
+
+export default User;
